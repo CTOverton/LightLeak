513f4547cec06d25e9b2ea11878925f5bf8d89a0
Christian <c@ctoverton.com>
2020-01-06 23:45:10 -0500

    Perlin Generation Example

---
+++ Assets/PerlinGenerator.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class PerlinGenerator : MonoBehaviour
+{
+    public int perlinTextureSizeX;
+    public int perlinTextureSizeY;
+    public bool randomizeNoiseOffset;
+    public Vector2 perlinOffset;
+    public float noiseScale = 1f;
+    public int perlinGridStepSizeX = 4;
+    public int perlinGridStepSizeY = 4;
+
+    public bool visualizeGrid = false;
+    public GameObject visualizationCube;
+    public float visualizationHeightScale = 5f;
+    public RawImage visualizationUI;
+
+
+    private Texture2D perlinTexture;
+
+    private void Start()
+    {
+        Generate();
+    }
+
+    public void Generate()
+    {
+        GenerateNoise();
+        if (visualizeGrid)
+        {
+            VisualizeGrid();
+        }
+    }
+    
+    private void GenerateNoise()
+    {
+        if (randomizeNoiseOffset)
+        {
+            perlinOffset = new Vector2(Random.Range(0,99999),Random.Range(0,99999));
+        }
+        
+        perlinTexture = new Texture2D(perlinTextureSizeX, perlinTextureSizeY);
+
+        for (int x = 0; x < perlinTextureSizeX; x++)
+        {
+            for (int y = 0; y < perlinTextureSizeY; y++)
+            {
+                perlinTexture.SetPixel(x,y,SampleNoise(x,y));
+            }
+        }
+        
+        perlinTexture.Apply();
+        visualizationUI.texture = perlinTexture;
+    }
+
+    private Color SampleNoise(int x, int y)
+    {
+        float xCoord = (float) x / perlinTextureSizeX * noiseScale + perlinOffset.x;
+        float yCoord = (float) y / perlinTextureSizeY * noiseScale + perlinOffset.y;
+
+        float sample = Mathf.PerlinNoise(xCoord, yCoord);
+        Color perlinColor = new Color(sample, sample, sample);
+
+        return perlinColor;
+    }
+
+    private void VisualizeGrid()
+    {
+        GameObject visualizationParent = new GameObject("VisualizationParent");
+        visualizationParent.transform.SetParent(this.transform);
+
+        for (int x = 0; x < perlinGridStepSizeX; x++)
+        {
+            for (int y = 0; y < perlinGridStepSizeY; y++)
+            {
+                float i = SampleStepped(x, y) * visualizationHeightScale;
+                
+                GameObject clone = Instantiate(visualizationCube, 
+                    new Vector3(x, i, y) 
+                    + transform.position, transform.rotation);
+
+                clone.transform.localScale = new Vector3(1,i,1);
+                clone.transform.SetParent(visualizationParent.transform);
+                //GeneratedObjectControl.instance.AddObject(clone);
+            }
+        }
+
+        visualizationParent.transform.position = 
+            new Vector3(-perlinGridStepSizeX * .5f, -visualizationHeightScale * .5f, -perlinGridStepSizeY * .5f);
+    }
+
+    private float SampleStepped(int x, int y)
+    {
+        int gridStepSizeX = perlinTextureSizeX / perlinGridStepSizeX;
+        int gridStepSizeY = perlinTextureSizeY / perlinGridStepSizeY;
+        
+        float sampleFloat = perlinTexture.GetPixel(Mathf.FloorToInt(x * gridStepSizeX), Mathf.FloorToInt(y * gridStepSizeX)).grayscale;
+        
+        return sampleFloat;
+    }
+}
---
+++ Assets/ZoneController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneController : MonoBehaviour
+{
+    public Vector2 id;
+    public string type;
+    public int size;
+    public Vector2 origin;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
