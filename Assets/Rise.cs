b47bcfaa165e5b716f541c628e3d8c293448390f
Christian <c@ctoverton.com>
2020-01-07 23:33:31 -0500

    Animated Spawn

---
+++ Assets/Rise.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rise : MonoBehaviour
+{
+    private float destination;
+    void Start()
+    {
+        destination = transform.position.y;
+        transform.position -= new Vector3(0,10,0);
+    }
+
+    
+    void Update()
+    {
+        transform.position += new Vector3(0,10f,0) * Time.deltaTime;
+
+        if (transform.position.y > destination)
+        {
+            enabled = false;
+        }
+    }
+}
